23a03bc9e53be3cfff9c885c67037b1ace9ce8fb
vspdontukurthi <vsp@hotmail.co.uk>
2017-12-16 22:21:41 +0000

    test build.

--- src/test/java/feature/salary/SalaryTest.java
+++ src/test/java/feature/salary/SalaryTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+//test for salary
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber", "json:target/salary-cucumber.json" }) 
 public class SalaryTest {
